bd0d3141d6baf7175f79724d1c67d3fbd88840ac
NikaTheDev <161513904+NikaTheDev@users.noreply.github.com>
2026-04-23 19:43:20 +0400

    add basic structure components&routes, reset styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { PageNotFound } from './pages/page-not-found/page-not-found';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home, pathMatch: 'full' },
+
+  {
+    path: 'about',
+    loadComponent: () => import('./pages/about/about').then((m) => m.About),
+    title: 'Project - About',
+  },
+
+  {
+    path: 'contact',
+    loadComponent: () => import('./pages/contact/contact').then((m) => m.Contact),
+    title: 'Project - Contact',
+  },
+
+  { path: '**', component: PageNotFound },
+];
---
+++ src/app/pages/auth/components/password-recovery/password-recovery.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-password-recovery',
+  imports: [],
+  templateUrl: './password-recovery.html',
+  styleUrl: './password-recovery.css',
+})
+export class PasswordRecovery {}
