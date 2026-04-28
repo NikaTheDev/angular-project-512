2549da13ba00c0068e545eb99a17465ab64ce74d
NikaTheDev <161513904+NikaTheDev@users.noreply.github.com>
2026-04-28 16:48:36 +0400

    add header

--- src/app/core/header/header.ts
+++ src/app/core/header/header.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Logo } from "../../shared/components/logo/logo";
+import { Nav } from "../../shared/components/nav/nav";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [Logo, Nav],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
